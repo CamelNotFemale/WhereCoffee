8f8b4f28360de343479c4f1ec1806de2ddf034e5
bubulkka <anastisch@gmail.com>
2022-04-14 19:55:42 +0300

    Added sorting order for coffee shops on the page

--- Where-Coffee-backend-app/src/main/java/com/github/nazzrrg/wherecoffeeapplication/service/CafeService.java
+++ Where-Coffee-backend-app/src/main/java/com/github/nazzrrg/wherecoffeeapplication/service/CafeService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -124,7 +125,7 @@ public class CafeService {
         }
     }
     public Page<Cafe> getPage(int page, String location, Double dist, boolean confirmed) {
-        Pageable pageable = PageRequest.of(page, itemsOnPage);
+        Pageable pageable = PageRequest.of(page, itemsOnPage, Sort.by(Sort.Direction.ASC, "id"));
         if (confirmed) {
             // точка центра поиска
             Double lat = Double.parseDouble(location.split(",")[0]);
